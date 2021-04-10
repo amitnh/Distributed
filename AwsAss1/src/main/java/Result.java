7be0da82d647a73b397156b7c90685ffce574dc8
amit <amitnh@gmail.com>
2021-04-10 07:32:08 -0700

    worker update, 2 new classes

--- AwsAss1/src/main/java/Result.java
+++ AwsAss1/src/main/java/Result.java
@@ -4,9 +4,14 @@ public class Result {
     protected int Reviewindex;
     //TODO
     protected int sentiment;
-    protected String result;
+    protected String entities;
 
     public void setSentimentAnalysis(int sentiment) {
         this.sentiment=sentiment;
     }
+    public void setNamedEntityRecognition(String entities) {
+        this.entities=entities;
+    }
+
+
 }
