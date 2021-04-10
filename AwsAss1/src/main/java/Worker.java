f67d228e40747a913b5d6ef12d936e0fac43bd22
amit <amitnh@gmail.com>
2021-04-10 06:51:17 -0700

    worker update

--- AwsAss1/src/main/java/Worker.java
+++ AwsAss1/src/main/java/Worker.java
@@ -1,19 +1,95 @@
+import java.util.List;
+import java.util.Properties;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
 public class Worker {
 
+    static sentimentAnalysisHandler sentimentAnalysisHandler = new sentimentAnalysisHandler();
+    static namedEntityRecognitionHandler namedEntityRecognitionHandler = new namedEntityRecognitionHandler();
 
     public static void main(String[] args) {
-    WORK();
-    }
-
-    private static void WORK() {
         while(true){
             //worker pulls a review from reviews_SQS added by the manager, performs necessary algorithms, and returns the result to the manager via results_SQS
             Review review = pullReviewFromSQS(); //maybe array of reviews to work on
-            Result result = ProccesReview();
+            Result result = ProccesReview(review);
             //push the result to results_SQS for the manager to continue process it
             pushResultToSQS(result);
             // remove the review from the jobs_SQS
             removeJobFromSQS(review);
         }
+
+
     }
+
+    private static Result ProccesReview(Review review) {
+        Result result = new Result();
+
+        //init Sentiment
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP sentimentPipeline = new StanfordCoreNLP(props);
+        //init Entities
+        props = new Properties();
+        props.put("annotators", "tokenize , ssplit, pos, lemma, ner");
+        StanfordCoreNLP NERPipeline = new StanfordCoreNLP(props);
+
+        String reviewStr = review.getReview();
+        result.setSentimentAnalysis = findSentiment(reviewStr);
+        result.setNamedEntityRecognition = printEntities(reviewStr);
+        return result;
+    }
+
+    public static int findSentiment(String review) {
+        int mainSentiment = 0;
+        if (review!= null && review.length() > 0) {
+            int longest = 0;
+            Annotation annotation = sentimentPipeline.process(review);
+            for (CoreMap sentence : annotation
+                    .get(CoreAnnotations.SentencesAnnotation.class)) {
+                Tree tree = sentence.get( SentimentCoreAnnotations.AnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+            }
+        }
+        return mainSentiment;
+    }
+
+
+    public static void printEntities(String review){
+// create an empty Annotation just with the given text
+        Annotation document = new Annotation(review);
+// run all Annotators on this text
+        NERPipeline.annotate(document);
+// these are all the sentences in this document
+// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+        for(CoreMap sentence: sentences) {
+// traversing the words in the current sentence
+// a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+// this is the text of the token
+                String word = token.get(TextAnnotation.class);
+// this is the NER label of the token
+                String ne = token.get(NamedEntityTagAnnotation.class);
+                System.out.println("\t-" + word + ":" + ne);
+            }
+        }
+    }
+
+
 }
